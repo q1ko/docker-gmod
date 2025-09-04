d7d1a2fd98032e08144a53f748815f96d664e897
q1ko <71758869+q1ko@users.noreply.github.com>
2025-09-05 02:42:09 +1000

    Add files via upload

---
+++ ttt/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+SERVER_DIR="/gmod/server"
+STEAMCMD="/usr/games/steamcmd"   # Correct for steamcmd/steamcmd image
+
+# Default values (override with -e VAR=value)
+APP_ID=4020
+MAXPLAYERS=${MAXPLAYERS:-32}
+MAP=${MAP:-ttt_minecraft_b5}
+GAME_MODE=${GAME_MODE:-terrortown}
+COLLECTION=${COLLECTION:-3561782742}
+
+echo "=== Updating/Installing Garry's Mod server ==="
+"$STEAMCMD" \
+  +force_install_dir "$SERVER_DIR" \
+  +login anonymous \
+  +app_update "$APP_ID" validate \
+  +quit
+
+echo "=== Starting Garry's Mod Server ==="
+cd "$SERVER_DIR"
+exec ./srcds_run -game garrysmod \
+    -console \
+    +r_hunkalloclightmaps 0 \
+    +maxplayers "$MAXPLAYERS" \
+    +map "$MAP" \
+    +gamemode "$GAME_MODE" \
+    +host_workshop_collection "$COLLECTION"
